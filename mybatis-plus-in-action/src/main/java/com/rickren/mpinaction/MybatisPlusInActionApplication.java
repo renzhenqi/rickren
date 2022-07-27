a8a37906f9670e918b2ac307fae9166e1379091e
renzhenqi <qinze5577@gmail.com>
2022-07-27 13:32:28 +0800

    local reset

--- mybatis-plus-in-action/src/main/java/com/rickren/mpinaction/MybatisPlusInActionApplication.java
+++ mybatis-plus-in-action/src/main/java/com/rickren/mpinaction/MybatisPlusInActionApplication.java
@@ -3,12 +3,13 @@ package com.rickren.mpinaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @RestController
-@MapperScan("com.rickren.mpinaction/**/mapper*")
+@MapperScan("com.rickren.mpinaction.**.mapper")
 public class MybatisPlusInActionApplication {
 
 	public static void main(String[] args) {
--- mybatis-plus-in-action/src/test/java/com/rickren/mpinaction/Jdk17InActionApplicationTests.java
+++ mybatis-plus-in-action/src/test/java/com/rickren/mpinaction/Jdk17InActionApplicationTests.java
@@ -8,6 +8,7 @@ class Jdk17InActionApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println(111);
 	}
 
 }
